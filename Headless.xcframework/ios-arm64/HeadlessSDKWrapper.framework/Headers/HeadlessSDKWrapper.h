12dc5b517c6c6eb9de7aa2e64be84eb5c36d528f
Mario Gamal <mario.gamal@minesecsoftpos.com>
2025-12-09 11:44:20 +0200

    Add Headless binary package v0.1.0

---
+++ Headless.xcframework/ios-arm64/HeadlessSDKWrapper.framework/Headers/HeadlessSDKWrapper.h
@@ -0,0 +1,18 @@
+//
+//  HeadlessSDKWrapper.h
+//  HeadlessSDKWrapper
+//
+//  Created by SONGYAO on 2025/11/27.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HeadlessSDKWrapper.
+FOUNDATION_EXPORT double HeadlessSDKWrapperVersionNumber;
+
+//! Project version string for HeadlessSDKWrapper.
+FOUNDATION_EXPORT const unsigned char HeadlessSDKWrapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HeadlessSDKWrapper/PublicHeader.h>
+
+
